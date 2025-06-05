8bba03eeb4a296c27b8eb0cb7839357f839e6cd8
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-05 15:03:13 -0400

    feat(client:domain): add ClientNotFoundException for handling client not found errors
    Ref: ZV-1

---
+++ src/main/java/com/zoolandia/app/features/client/service/exception/ClientNotFoundException.java
@@ -0,0 +1,7 @@
+package com.zoolandia.app.features.client.service.exception;
+
+public class ClientNotFoundException extends RuntimeException {
+    public ClientNotFoundException(Long id) {
+        super("Cliente no encontrado con ID: " + id);
+    }
+}
