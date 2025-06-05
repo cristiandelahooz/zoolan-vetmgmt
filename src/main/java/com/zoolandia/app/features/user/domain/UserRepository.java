0a7b1d5fe8df8da6701cd4afa8210fa5cd5e3ab3
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-05 15:03:34 -0400

    feat(user:domain): refactor UserRepository to extend AbstractRepository and add repository annotation
    Ref: ZV-1

--- src/main/java/com/zoolandia/app/features/user/domain/UserRepository.java
+++ src/main/java/com/zoolandia/app/features/user/domain/UserRepository.java
@@ -1,11 +1,14 @@
 package com.zoolandia.app.features.user.domain;
 
+import com.zoolandia.app.common.entity.AbstractRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+@Repository
+public interface UserRepository extends AbstractRepository<User, Long> {
 	Optional<User> findByUsername(String username);
 	Optional<User> findByEmail(String email);
 }
