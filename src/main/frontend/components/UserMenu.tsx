14f1c52ba8248c6607817e5d5390cdc34f1bbbea
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-07 18:36:59 -0400

    feat: implement UserMenu component with avatar and menu items
    Ref: ZV-1

---
+++ src/main/frontend/components/UserMenu.tsx
@@ -0,0 +1,46 @@
+import {
+    Avatar,
+    MenuBar, MenuBarItem,
+    MenuBarItemSelectedEvent
+} from "@vaadin/react-components";
+
+type UserMenuItem = MenuBarItem<{ action?: () => void }>
+
+function UserMenu() {
+    // TODO Replace with real user information and actions
+    const items: Array<UserMenuItem> = [
+        {
+            component: (
+                <>
+                    <Avatar theme="xsmall" name="Sindy" colorIndex={5}
+                            className="mr-s"/>
+                    Sindy
+                </>
+            ),
+            children: [
+                {
+                    text: 'View Profile',
+                    disabled: true,
+                    action: () => console.log('View Profile')
+                },
+                {
+                    text: 'Manage Settings',
+                    disabled: true,
+                    action: () => console.log('Manage Settings')
+                },
+                {
+                    text: 'Logout',
+                    disabled: true,
+                    action: () => console.log('Logout')
+                }
+            ]
+        }
+    ]
+    const onItemSelected = (event: MenuBarItemSelectedEvent<UserMenuItem>) => {
+        event.detail.value.action?.()
+    }
+    return <MenuBar theme="tertiary-inline" items={items}
+                    onItemSelected={onItemSelected} className="m-m"
+                    slot="drawer"/>
+}
+
