fd0351a662ca3ea833164a2243eaafb7f798b8bc
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-08 19:27:17 -0400

    refactor(MainLayout): clean up imports and remove unused Header component
    Ref: ZV-1

--- src/main/frontend/views/@layout.tsx
+++ src/main/frontend/views/@layout.tsx
@@ -1,74 +1,40 @@
-import {
-	AppLayout,
-	Icon,
-	ProgressBar,
-	Scroller,
-	SideNav,
-	SideNavItem,
-} from "@vaadin/react-components";
+import { AppLayout, DrawerToggle, Icon, ProgressBar, Scroller, SideNav, SideNavItem } from "@vaadin/react-components";
+import { DropdownMenuRadioGroupDemo } from "Frontend/components/DropdownMenuRadioGroupDemo";
 import { UserMenu } from "Frontend/components/UserMenu";
-import { ViewToolbar } from "Frontend/components/ViewToolbar";
-import {
-	type MenuItemConfig,
-	menuConfig,
-} from "Frontend/components/config/menuConfig";
-import { Suspense, useState } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router";
-
-function Header({ isSideNavOpened }: { isSideNavOpened: boolean }) {
-	// TODO Replace with real application logo and name
-	return (
-		<>
-			<div
-				className="flex items-center"
-				{...(isSideNavOpened ? { slot: "drawer" } : {})}
-			>
-				<ViewToolbar title={isSideNavOpened ? "Zoolandia" : ""} />
-			</div>
-		</>
-	);
-}
+import { type MenuItemConfig, menuConfig } from "Frontend/components/config/menuConfig";
+import { Suspense } from "react";
+import { NavLink, Outlet, useLocation, useNavigate } from "react-router";
 
 function MainMenu() {
 	const navigate = useNavigate();
 	const location = useLocation();
 
 	return (
-		<SideNav
-			className="mx-m"
-			onNavigate={({ path }) => path != null && navigate(path)}
-			location={location}
-		>
+		<SideNav className="mx-m" onNavigate={({ path }) => path != null && navigate(path)} location={location}>
 			{menuConfig.map(({ path, icon, title, children }: MenuItemConfig) => (
 				<SideNavItem path={path} key={path}>
 					{icon && <Icon icon={icon} className="mr-s" slot="prefix" />}
 					{title}
 					{children?.map((child: MenuItemConfig) => (
 						<SideNavItem path={child.path} key={child.path} slot="children">
-							{child.icon && (
-								<Icon icon={child.icon} className="mr-s" slot="prefix" />
-							)}
+							{child.icon && <Icon icon={child.icon} className="mr-s" slot="prefix" />}
 							{child.title}
 						</SideNavItem>
 					))}
 				</SideNavItem>
 			))}
+			<DropdownMenuRadioGroupDemo />
 		</SideNav>
 	);
 }
 
 export default function MainLayout() {
-	const [isSideNavOpened, setSideNavOpened] = useState(true);
-
-	const handleDrawerToggle: () => void = () => {
-		setSideNavOpened(!isSideNavOpened);
-	};
 	return (
-		<AppLayout
-			primarySection="drawer"
-			onDrawerOpenedChanged={handleDrawerToggle}
-		>
-			<Header isSideNavOpened={isSideNavOpened} />
+		<AppLayout>
+			<DrawerToggle slot="navbar" />
+			<NavLink to="/" slot="navbar">
+				Zoolandia
+			</NavLink>
 			<Scroller slot="drawer">
 				<MainMenu />
 			</Scroller>
