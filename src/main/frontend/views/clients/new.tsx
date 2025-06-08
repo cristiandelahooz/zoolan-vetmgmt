2bad47efff7b99e84840f80dae49a28275f9057e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-08 19:27:28 -0400

    refactor(Register): replace _ClientEntryForm with AutoForm for client registration
    Ref: ZV-1

--- src/main/frontend/views/clients/new.tsx
+++ src/main/frontend/views/clients/new.tsx
@@ -1,5 +1,9 @@
 import type { ViewConfig } from "@vaadin/hilla-file-router/types.js";
-import _ClientEntryForm from "Frontend/views/clients/_ClientEntryForm";
+import { AutoForm } from "@vaadin/hilla-react-crud";
+import { PasswordField } from "@vaadin/react-components";
+import ClientCreateDTOModel from "Frontend/generated/com/zoolandia/app/features/client/service/dto/ClientCreateDTOModel";
+import { ClientServiceImpl } from "Frontend/generated/endpoints";
+import type React from "react";
 
 export const config: ViewConfig = {
 	title: "Registrar Cliente",
@@ -9,7 +13,15 @@ export default function Register() {
 	return (
 		<>
 			<main className="w-full h-full flex flex-col box-border gap-s p-m">
-				<_ClientEntryForm />
+				<AutoForm
+					service={ClientServiceImpl}
+					model={ClientCreateDTOModel}
+					fieldOptions={{
+						password: {
+							renderer: ({ field }) => <PasswordField {...field} />,
+						},
+					}}
+				/>
 			</main>
 		</>
 	);
