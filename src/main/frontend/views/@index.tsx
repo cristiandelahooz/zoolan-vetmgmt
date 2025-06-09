10c341545f1e3fc53f690917dd6e0a2d6149ef13
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-08 23:41:10 -0400

    chore(biome): format

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,46 +1,46 @@
-import type { ViewConfig } from "@vaadin/hilla-file-router/types.js";
-import { Chart } from "@vaadin/react-components-pro/Chart";
-import { ChartSeries } from "@vaadin/react-components-pro/ChartSeries";
+import type { ViewConfig } from '@vaadin/hilla-file-router/types.js'
+import { Chart } from '@vaadin/react-components-pro/Chart'
+import { ChartSeries } from '@vaadin/react-components-pro/ChartSeries'
 
 export const config: ViewConfig = {
-	menu: {
-		exclude: true,
-	},
-};
+  menu: {
+    exclude: true,
+  },
+}
 
 export default function MainView() {
-	return (
-		<main className="w-full h-full">
-			<Chart type="bubble" title="" style={{ width: "100%" }}>
-				<ChartSeries
-					title="Zoolandia"
-					values={[
-						{ x: 1, y: 2, z: 2 },
-						{ x: 2, y: 4, z: 1 },
-						{ x: 3, y: 6, z: 4 },
-						{ x: 1, y: 5, z: 3 },
-					]}
-				/>
-			</Chart>
-			<Chart
-				type="line"
-				title="Ventas por mes (2025)"
-				additionalOptions={{
-					xAxis: {
-						categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-					},
-					yAxis: { title: { text: "DOP ()" } },
-				}}
-			>
-				<ChartSeries
-					title="Neurovet Perros"
-					values={[42112, 58698, 12276, 33202, 74518, 45498, 42477, 17896, 44297, 22456, 38547, 12621]}
-				/>
-				<ChartSeries
-					title="C Fagia Gatos"
-					values={[70972, 48589, 94434, 58270, 77282, 7108, 54085, 44401, 28868, 79643, 14383, 76036]}
-				/>
-			</Chart>
-		</main>
-	);
+  return (
+    <main className="w-full h-full">
+      <Chart type="bubble" title="" style={{ width: '100%' }}>
+        <ChartSeries
+          title="Zoolandia"
+          values={[
+            { x: 1, y: 2, z: 2 },
+            { x: 2, y: 4, z: 1 },
+            { x: 3, y: 6, z: 4 },
+            { x: 1, y: 5, z: 3 },
+          ]}
+        />
+      </Chart>
+      <Chart
+        type="line"
+        title="Ventas por mes (2025)"
+        additionalOptions={{
+          xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          },
+          yAxis: { title: { text: 'DOP ()' } },
+        }}
+      >
+        <ChartSeries
+          title="Neurovet Perros"
+          values={[42112, 58698, 12276, 33202, 74518, 45498, 42477, 17896, 44297, 22456, 38547, 12621]}
+        />
+        <ChartSeries
+          title="C Fagia Gatos"
+          values={[70972, 48589, 94434, 58270, 77282, 7108, 54085, 44401, 28868, 79643, 14383, 76036]}
+        />
+      </Chart>
+    </main>
+  )
 }
--- src/main/frontend/views/@layout.tsx
+++ src/main/frontend/views/@layout.tsx
@@ -1,50 +1,49 @@
-import { AppLayout, DrawerToggle, Icon, ProgressBar, Scroller, SideNav, SideNavItem } from "@vaadin/react-components";
-import { DropdownMenuRadioGroupDemo } from "Frontend/components/DropdownMenuRadioGroupDemo";
-import { UserMenu } from "Frontend/components/UserMenu";
-import { type MenuItemConfig, menuConfig } from "Frontend/components/config/menuConfig";
-import { Suspense } from "react";
-import { NavLink, Outlet, useLocation, useNavigate } from "react-router";
+import { AppLayout, DrawerToggle, Icon, ProgressBar, Scroller, SideNav, SideNavItem } from '@vaadin/react-components'
+import { UserMenu } from 'Frontend/components/UserMenu'
+import { type MenuItemConfig, menuConfig } from 'Frontend/components/config/menuConfig'
+import { Suspense } from 'react'
+import { NavLink, Outlet, useLocation, useNavigate } from 'react-router'
 
 function MainMenu() {
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate()
+  const location = useLocation()
 
-	return (
-		<SideNav
-			className="mx-m layout-side-nav"
-			onNavigate={({ path }) => path != null && navigate(path)}
-			location={location}
-		>
-			{menuConfig.map(({ path, icon, title, children }: MenuItemConfig) => (
-				<SideNavItem path={path} key={path}>
-					{icon && <Icon icon={icon} className="mr-s" slot="prefix" />}
-					{title}
-					{children?.map((child: MenuItemConfig) => (
-						<SideNavItem path={child.path} key={child.path} slot="children">
-							{child.icon && <Icon icon={child.icon} className="mr-s" slot="prefix" />}
-							{child.title}
-						</SideNavItem>
-					))}
-				</SideNavItem>
-			))}
-		</SideNav>
-	);
+  return (
+    <SideNav
+      className="mx-m layout-side-nav"
+      onNavigate={({ path }) => path != null && navigate(path)}
+      location={location}
+    >
+      {menuConfig.map(({ path, icon, title, children }: MenuItemConfig) => (
+        <SideNavItem path={path} key={path}>
+          {icon && <Icon icon={icon} className="mr-s" slot="prefix" />}
+          {title}
+          {children?.map((child: MenuItemConfig) => (
+            <SideNavItem path={child.path} key={child.path} slot="children">
+              {child.icon && <Icon icon={child.icon} className="mr-s" slot="prefix" />}
+              {child.title}
+            </SideNavItem>
+          ))}
+        </SideNavItem>
+      ))}
+    </SideNav>
+  )
 }
 
 export default function MainLayout() {
-	return (
-		<AppLayout>
-			<DrawerToggle slot="navbar" />
-			<NavLink to="/" slot="navbar">
-				Zoolandia
-			</NavLink>
-			<Scroller slot="drawer">
-				<MainMenu />
-			</Scroller>
-			<UserMenu />
-			<Suspense fallback={<ProgressBar indeterminate={true} className="m-0" />}>
-				<Outlet />
-			</Suspense>
-		</AppLayout>
-	);
+  return (
+    <AppLayout>
+      <DrawerToggle slot="navbar" />
+      <NavLink to="/" slot="navbar">
+        Zoolandia
+      </NavLink>
+      <Scroller slot="drawer">
+        <MainMenu />
+      </Scroller>
+      <UserMenu />
+      <Suspense fallback={<ProgressBar indeterminate={true} className="m-0" />}>
+        <Outlet />
+      </Suspense>
+    </AppLayout>
+  )
 }
