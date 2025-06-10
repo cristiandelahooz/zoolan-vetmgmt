cfef629877e139bb374e3abb0b65c2dc03406a07
Carolina <cbencome@gmail.com>
2025-06-10 17:47:40 -0400

    feat(pet): change field names to spanish
    Ref: ZV-33

--- src/main/frontend/views/pet/view.tsx
+++ src/main/frontend/views/pet/view.tsx
@@ -4,14 +4,25 @@ import { PetServiceImpl } from "Frontend/generated/endpoints";
 import PetModel from "Frontend/generated/com/zoolandia/app/features/pet/domain/PetModel";
 
 export const config: ViewConfig = {
-    menu: { title: 'Ver' },
-    title: 'Lista de Mascotas',
+    menu: { title: "Ver" },
+    title: "Lista de Mascotas",
 };
 
 export default function PetListView() {
     return (
         <main className="w-full h-full flex flex-col box-border gap-s p-m">
-            <AutoGrid service={PetServiceImpl} model={PetModel} />
+            <AutoGrid
+                service={PetServiceImpl}
+                model={PetModel}
+                columnOptions={{
+                    name: { header: "Nombre" },
+                    type: { header: "Tipo" },
+                    breed: { header: "Raza" },
+                    gender: { header: "Género" },
+                    birthDate: { header: "Fecha de Nacimiento" },
+                    owner: { header: "Dueño" },
+                }}
+            />
         </main>
     );
 }
