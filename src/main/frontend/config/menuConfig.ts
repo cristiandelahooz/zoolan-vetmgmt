45cc798a2103fc68abfadf91541fcf232595db06
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-10 23:13:20 -0400

    feat(menu): add menu configuration for navigation items

--- src/main/frontend/config/menuConfig.ts
+++ src/main/frontend/config/menuConfig.ts
@@ -1,10 +1,12 @@
 import type { IconProps } from '@vaadin/react-components'
 import { ROUTES } from 'Frontend/lib/constants/routes'
+import petsIcon from 'Frontend/lib/icons/pets-svgrepo-com.svg'
 
 export interface MenuItemConfig {
   path: string | undefined
   title: string
   icon?: IconProps['icon']
+  src?: string | undefined
   children?: MenuItemConfig[]
 }
 
@@ -21,6 +23,23 @@ export const menuConfig: MenuItemConfig[] = [
     children: [
       { path: ROUTES.CLIENTS, title: 'Listar', icon: 'lumo:align-left' },
       { path: ROUTES.CLIENT_REGISTER, title: 'Registrar', icon: 'lumo:edit' },
+    ],
+  },
+  {
+    path: undefined,
+    title: 'Mascotas',
+    src: petsIcon,
+    children: [
+      {
+        path: ROUTES.PETS,
+        title: 'Listar',
+        icon: 'lumo:align-left',
+      },
+      {
+        path: ROUTES.PET_REGISTER,
+        title: 'Registrar',
+        icon: 'lumo:edit',
+      },
     ],
   },
 ]
