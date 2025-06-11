aef0468124168d134761ede903607cd3ca95dfa2
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-06-11 11:39:11 -0400

    feat(employees): refactor AutoGrid column options to use constants for better maintainability
    Ref: ZV-29

--- src/main/frontend/views/employees/@index.tsx
+++ src/main/frontend/views/employees/@index.tsx
@@ -3,70 +3,13 @@ import  EmployeeModel from 'Frontend/generated/com/zoolandia/app/features/employ
 import { EmployeeServiceImpl } from 'Frontend/generated/endpoints';
 import EmployeeCreateDTOModel
     from "Frontend/generated/com/zoolandia/app/features/employee/service/dto/EmployeeCreateDTOModel";
-import {PasswordField} from "@vaadin/react-components";
+import {AUTO_GRID_EMPLOYEE_FIELD_OPTIONS} from "Frontend/lib/constants/employee-field-config";
 
 export default function EmployeesView() {
-    //return <AutoGrid service={EmployeeServiceImpl} model={EmployeeModel} />;
     return (
             <main className="w-full h-full flex flex-col box-border gap-s p-m">
                 <AutoGrid service={EmployeeServiceImpl} model={EmployeeCreateDTOModel}
-                          columnOptions={{
-                              username: {
-                                  header: 'Nombre de Usuario',
-                              },
-                              password: {
-                                  renderer: ({ item }) => <span> ••••••</span>,
-                                  header: 'Contraseña',
-                              },
-                              email: {
-                                  header: 'Correo Electrónico',
-                              },
-                              firstName: {
-                                  header: 'Nombre',
-                              },
-                              lastName: {
-                                  header: 'Apellido',
-                              },
-                              phoneNumber: {
-                                  header: 'Número de Teléfono',
-                              },
-                              birthDate: {
-                                  header: 'Fecha de Nacimiento',
-                              },
-                              gender: {
-                                  header: 'Género',
-                              },
-                              nationality: {
-                                  header: 'Nacionalidad',
-                              },
-                              employeeRole: {
-                                  header: 'Rol del Empleado',
-                              },
-                              municipality: {
-                                  header: 'Municipio',
-                              },
-                              province: {
-                                  header: 'Provincia',
-                              },
-                              streetAddress: {
-                                  header: 'Calle'
-                              },
-                              hireDate: {
-                                  header: 'Fecha de Contratación',
-                              },
-                              salary: {
-                                  header: 'Salario',
-                              },
-                              notes: {
-                                  header: 'Notas',
-                              },
-                              available: {
-                                  header: 'Disponible',
-                              },
-                              active: {
-                                  header: 'Activo',
-                              },
-                          }}
+                          columnOptions={AUTO_GRID_EMPLOYEE_FIELD_OPTIONS}
                 />
             </main>
     );
