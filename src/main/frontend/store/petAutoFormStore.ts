998b249281f62b9a622c05894b11e1dd36ce678a
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-16 23:44:25 -0400

    feat: add zustand store for pet form management
    Ref: ZV-33

---
+++ src/main/frontend/store/petAutoFormStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+
+type PetFormState = {
+  name: string
+  type: string
+  breed: string
+  birthDate: string
+  gender: string
+  ownerId?: number
+  setField: <K extends keyof PetFormState>(field: K, value: PetFormState[K]) => void
+  reset: () => void
+}
+
+export const usePetFormStore = create<PetFormState>((set) => ({
+  name: '',
+  type: '',
+  breed: '',
+  birthDate: '',
+  gender: '',
+  ownerId: undefined,
+  setField: (field, value) => set((state) => ({ ...state, [field]: value })),
+  reset: () =>
+    set({
+      name: '',
+      type: '',
+      breed: '',
+      birthDate: '',
+      gender: '',
+      ownerId: undefined,
+      setField: () => {},
+      reset: () => {},
+    }),
+}))
