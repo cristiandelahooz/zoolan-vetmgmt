a696249f87deca039ca0fe42a9b0a8af1945bca7
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-17 00:00:29 -0400

    refactor(pet): update DTOS to use String for breed and add validation annotation
    Ref: ZV-33

--- src/main/java/com/zoolandia/app/features/pet/service/dto/PetUpdateDTO.java
+++ src/main/java/com/zoolandia/app/features/pet/service/dto/PetUpdateDTO.java
@@ -1,14 +1,15 @@
 package com.zoolandia.app.features.pet.service.dto;
 
 import com.zoolandia.app.features.pet.domain.Gender;
-import com.zoolandia.app.features.pet.domain.PetBreed;
 import com.zoolandia.app.features.pet.domain.PetType;
+import com.zoolandia.app.features.pet.validation.ValidPetBreedDTO;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@ValidPetBreedDTO
 public class PetUpdateDTO {
 
     @Size(max = 100, message = "El nombre no puede exceder 100 caracteres")
@@ -16,7 +17,7 @@ public class PetUpdateDTO {
 
     private PetType type;
 
-    private PetBreed breed;
+    private String breed;
 
     private LocalDate birthDate;
 
