eb5eba79641251ce23c08f563041d4795583b8b5
Carolina <cbencome@gmail.com>
2025-06-25 00:44:54 -0400

    feat(pet): refactor mapOwner method to map owner as a list of clients
    Ref: ZV-6

--- src/main/java/com/zoolandia/app/features/pet/mapper/PetMapperHelper.java
+++ src/main/java/com/zoolandia/app/features/pet/mapper/PetMapperHelper.java
@@ -7,16 +7,21 @@ import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 @Component
 public class PetMapperHelper {
 
     @Autowired
     private ClientRepository clientRepository;
 
-    @Named("mapOwner")
-    public Client mapOwner(Long ownerId) {
-        if (ownerId == null)
-            return null;
-        return clientRepository.findById(ownerId).orElseThrow(() -> new OwnerNotFoundException(ownerId));
+    @Named("mapOwnerAsList")
+    public List<Client> mapOwnerAsList(Long ownerId) {
+        if (ownerId == null) return Collections.emptyList();
+        Client client = clientRepository.findById(ownerId)
+                .orElseThrow(() -> new OwnerNotFoundException(ownerId));
+        return List.of(client);
     }
+
 }
