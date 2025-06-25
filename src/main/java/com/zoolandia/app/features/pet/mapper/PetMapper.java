5614fc27f379b410e9429b56a8ecee31eaf43e1b
Carolina <cbencome@gmail.com>
2025-06-25 00:44:38 -0400

    feat(pet): update mapping for owner to support multiple owners in PetMapper
    Ref: ZV-6

--- src/main/java/com/zoolandia/app/features/pet/mapper/PetMapper.java
+++ src/main/java/com/zoolandia/app/features/pet/mapper/PetMapper.java
@@ -9,11 +9,11 @@ import org.mapstruct.*;
 public interface PetMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "owner", source = "ownerId", qualifiedByName = "mapOwner")
+    @Mapping(target = "owners", source = "ownerId", qualifiedByName = "mapOwnerAsList")
     Pet toEntity(PetCreateDTO dto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "owner", source = "ownerId", qualifiedByName = "mapOwner")
+    @Mapping(target = "owners", source = "ownerId", qualifiedByName = "mapOwnerAsList")
     void updatePetFromDTO(PetUpdateDTO dto, @MappingTarget Pet pet);
 
     PetCreateDTO toCreateDTO(Pet savedPet);
