52411d4b8be92cab7bd9d4f46d80996dee90d92c
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-03 10:45:45 -0400

    feat: add methods for medical history management in MedicalHistoryService
    Ref: ZV-12

--- src/main/java/com/zoolandia/app/features/medicalHistory/service/MedicalHistoryService.java
+++ src/main/java/com/zoolandia/app/features/medicalHistory/service/MedicalHistoryService.java
@@ -7,10 +7,16 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
+
 @BrowserCallable
 @Validated
 @Transactional(propagation = Propagation.REQUIRES_NEW)
 public interface MedicalHistoryService {
     MedicalHistory findOrCreateByPet(Pet pet);
     MedicalHistory findByPetId(Long petId);
+
+    MedicalHistory getOrCreateMedicalHistory(Pet pet);
+    MedicalHistory updateMedicalHistory(MedicalHistory medicalHistory);
+    List<MedicalHistory> getConsultationsByPetId(Long petId);
 }
