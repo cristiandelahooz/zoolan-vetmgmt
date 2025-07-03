e3aeaed75f6618b8897cd0916fe97749ee02b6c1
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-02 21:03:31 -0400

    feat: extend ConsultationRepository to support JPA specifications
    Ref: ZV-12

--- src/main/java/com/zoolandia/app/features/consultation/repository/ConsultationRepository.java
+++ src/main/java/com/zoolandia/app/features/consultation/repository/ConsultationRepository.java
@@ -6,13 +6,14 @@ import com.zoolandia.app.features.employee.domain.Employee;
 import com.zoolandia.app.features.pet.domain.Pet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ConsultationRepository extends AbstractRepository<Consultation, Long> {
+public interface ConsultationRepository extends AbstractRepository<Consultation, Long>, JpaSpecificationExecutor<Consultation> {
     Optional<Consultation> findByPet(Pet pet);
 
     Optional<Consultation> findByVeterinarian(Employee veterinarian);
