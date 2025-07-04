1193183ce8d346737d93ac239130317937ff6510
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-07-04 01:47:38 -0400

    feat: refactor PetMapperHelper to use ClientService for owner mapping

--- src/main/java/com/zoolandia/app/features/pet/mapper/PetMapperHelper.java
+++ src/main/java/com/zoolandia/app/features/pet/mapper/PetMapperHelper.java
@@ -1,7 +1,7 @@
 package com.zoolandia.app.features.pet.mapper;
 
 import com.zoolandia.app.features.client.domain.Client;
-import com.zoolandia.app.features.client.repository.ClientRepository;
+import com.zoolandia.app.features.client.service.ClientService;
 import com.zoolandia.app.features.pet.service.exception.OwnerNotFoundException;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +11,12 @@ import org.springframework.stereotype.Component;
 public class PetMapperHelper {
 
     @Autowired
-    private ClientRepository clientRepository;
+    private ClientService clientService;
 
     @Named("mapOwner")
     public Client mapOwner(Long ownerId) {
         if (ownerId == null)
             return null;
-        return clientRepository.findById(ownerId).orElseThrow(() -> new OwnerNotFoundException(ownerId));
+        return clientService.getClientById(ownerId).orElseThrow(() -> new OwnerNotFoundException(ownerId));
     }
 }
