8b6f59968878e7ab80e82d0637c3d2b6541a8956
carolinabencosme <cbencome@gmail.com>
2025-07-10 23:26:59 -0400

    feat(Product): add initial Product entity class with attributes and validations
    Ref: ZV-23

---
+++ src/main/java/com/wornux/data/entity/Product.java
@@ -0,0 +1,47 @@
+package com.wornux.data.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.wornux.data.enums.ProductCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+
+
+import java.math.BigDecimal;
+
+@Builder
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @Builder.Default
+    private boolean active = true;
+
+    @DecimalMin(value = "0.0", inclusive = true)
+    private BigDecimal price;
+
+    @Min(0)
+    private int stock;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+    @Enumerated(EnumType.STRING)
+    private ProductCategory category;
+}
