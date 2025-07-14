c13073c9d5999eb34645e082b5a0bc56a5633309
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-14 12:59:43 -0400

    feat(ProductRepository): add query for finding low stock products based on reorder level
    Ref: ZV-22

--- src/main/java/com/wornux/data/repository/ProductRepository.java
+++ src/main/java/com/wornux/data/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.wornux.data.repository;
 
 import com.wornux.data.base.AbstractRepository;
 import com.wornux.data.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.wornux.data.enums.ProductCategory;
@@ -21,5 +22,8 @@ public interface ProductRepository extends AbstractRepository<Product, Long> {
 
 
     List<Product> findByActiveTrue();
+
+    @Query("SELECT p FROM Product p WHERE p.stock <= p.reorderLevel AND p.active = true")
+    List<Product> findLowStockProducts();
 
 }
