c5f988f4a1aac9e6edbb9ff97fbcbd3244f5a891
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-07-17 09:16:17 -0400

    feat(toggle-button): implement ToggleButton component with customizable theme and label options

---
+++ src/main/java/com/wornux/components/ToggleButton.java
@@ -0,0 +1,40 @@
+package com.wornux.components;
+
+import com.vaadin.flow.component.HasTheme;
+import com.vaadin.flow.component.checkbox.Checkbox;
+
+/**
+ * @author me@fredpena.dev
+ * @created 12/01/2025  - 13:36
+ */
+public class ToggleButton extends Checkbox implements HasTheme {
+    public static final String THEME_NAME = "toggle-button";
+
+    public ToggleButton() {
+        this.addThemeName();
+    }
+
+    public ToggleButton(boolean initialValue) {
+        super(initialValue);
+        this.addThemeName();
+    }
+
+    public ToggleButton(String labelText, boolean initialValue) {
+        super(labelText, initialValue);
+        this.addThemeName();
+    }
+
+    public ToggleButton(String label, ValueChangeListener<ComponentValueChangeEvent<Checkbox, Boolean>> listener) {
+        super(label, listener);
+        this.addThemeName();
+    }
+
+    public ToggleButton(String labelText) {
+        super(labelText);
+        this.addThemeName();
+    }
+
+    private void addThemeName() {
+        this.addThemeName(THEME_NAME);
+    }
+}
