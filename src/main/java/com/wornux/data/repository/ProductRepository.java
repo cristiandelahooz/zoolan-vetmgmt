9cfdf64a577d361473f9a0a9a1b0730e590b3898
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-17 10:07:03 -0400

    refactor(ProductRepository): update interface to extend JpaRepository and JpaSpecificationExecutor
    Ref: ZV-22

--- src/main/java/com/wornux/data/repository/ProductRepository.java
+++ src/main/java/com/wornux/data/repository/ProductRepository.java
@@ -2,24 +2,21 @@ package com.wornux.data.repository;
 
 import com.wornux.data.base.AbstractRepository;
 import com.wornux.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.wornux.data.enums.ProductCategory;
 
-
 @Repository
-public interface ProductRepository extends AbstractRepository<Product, Long> {
-
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
     List<Product> findByNameContainingIgnoreCase(String name);
 
-
     List<Product> findByCategory(ProductCategory category);
 
-
     List<Product> findBySupplierId(Long supplierId);
-
 
     List<Product> findByActiveTrue();
 
