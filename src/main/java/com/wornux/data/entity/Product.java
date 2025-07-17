7588c994924331090590c4e8a31798e9ef0f372e
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-17 10:06:42 -0400

    refactor(Product): remove unnecessary blank line in imports
    Ref: ZV-22

--- src/main/java/com/wornux/data/entity/Product.java
+++ src/main/java/com/wornux/data/entity/Product.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 
-
 import java.math.BigDecimal;
 
 @Builder
