47780f93e7b8f9df9229bb59cf920063c2188feb
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-24 03:10:16 -0400

    fix: update EMPLOYEE_NEW route to use '/employees/form' for consistency

--- src/main/frontend/lib/constants/routes.ts
+++ src/main/frontend/lib/constants/routes.ts
@@ -27,7 +27,7 @@ export const ROUTES = {
   //Employee routes
   EMPLOYEES: '/employees',
   EMPLOYEE_EDIT: '/employees/edit',
-  EMPLOYEE_NEW: '/employees/new',
+  EMPLOYEE_NEW: '/employees/form',
 
   //waiting room routes
   WAITING_ROOM: '/waitingroom',
