35dc8dddea7603eb5b76974376831db7dbe48ecb
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-30 03:04:58 -0400

    refactor(employee-form): improve code formatting and readability in EmployeeForm class

--- src/main/java/com/wornux/views/employees/EmployeeForm.java
+++ src/main/java/com/wornux/views/employees/EmployeeForm.java
@@ -43,32 +43,32 @@ public class EmployeeForm extends Dialog {
     private final DatePicker birthDate = new DatePicker("Fecha de Nacimiento");
     private final ComboBox<Gender> gender = new ComboBox<>("Género");
     private final TextField nationality = new TextField("Nacionalidad");
-    
+
     // Address Information
     private final TextField province = new TextField("Provincia");
     private final TextField municipality = new TextField("Municipio");
     private final TextField sector = new TextField("Sector");
     private final TextField streetAddress = new TextField("Dirección");
-    
+
     // Employee Information
     private final ComboBox<EmployeeRole> employeeRole = new ComboBox<>("Rol");
     private final NumberField salary = new NumberField("Salario");
     private final DatePicker hireDate = new DatePicker("Fecha de Contratación");
     private final TextField workSchedule = new TextField("Horario Laboral");
-    
+
     // Emergency Contact
     private final TextField emergencyContactName = new TextField("Nombre de Contacto de Emergencia");
     private final TextField emergencyContactPhone = new TextField("Teléfono de Contacto de Emergencia");
-    
+
     private final Button saveButton = new Button("Guardar");
     private final Button cancelButton = new Button("Cancelar");
-    
+
     private final Binder<EmployeeCreateRequestDto> binder = new BeanValidationBinder<>(EmployeeCreateRequestDto.class);
     private transient EmployeeService employeeService;
-    
+
     @Setter
     private transient Runnable onSaveCallback;
-    
+
     private final List<Consumer<EmployeeCreateRequestDto>> employeeSavedListeners = new ArrayList<>();
     private final List<Runnable> employeeCancelledListeners = new ArrayList<>();
 
@@ -76,12 +76,12 @@ public class EmployeeForm extends Dialog {
 
     public EmployeeForm(EmployeeService employeeService) {
         this.employeeService = employeeService;
-        
+
         setHeaderTitle("Nuevo Empleado");
         setModal(true);
         setWidth("900px");
         setHeight("80vh");
-        
+
         createForm();
         setupValidation();
         setupEventListeners();
@@ -89,79 +89,55 @@ public class EmployeeForm extends Dialog {
 
     private void createForm() {
         FormLayout userInfo = new FormLayout();
-        userInfo.add(
-            username, password, firstName, lastName, email, phoneNumber, birthDate, gender, nationality
-        );
-        userInfo.setResponsiveSteps(
-            new FormLayout.ResponsiveStep("0", 1),
-            new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2)
-        );
-        
+        userInfo.add(username, password, firstName, lastName, email, phoneNumber, birthDate, gender, nationality);
+        userInfo.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2));
+
         FormLayout addressInfo = new FormLayout();
-        addressInfo.add(
-            province, municipality, sector, streetAddress
-        );
-        addressInfo.setResponsiveSteps(
-            new FormLayout.ResponsiveStep("0", 1),
-            new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2)
-        );
-        
+        addressInfo.add(province, municipality, sector, streetAddress);
+        addressInfo.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2));
+
         FormLayout employeeInfo = new FormLayout();
-        employeeInfo.add(
-            employeeRole, salary, hireDate, workSchedule
-        );
-        employeeInfo.setResponsiveSteps(
-            new FormLayout.ResponsiveStep("0", 1),
-            new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2)
-        );
-        
+        employeeInfo.add(employeeRole, salary, hireDate, workSchedule);
+        employeeInfo.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2));
+
         FormLayout emergencyInfo = new FormLayout();
-        emergencyInfo.add(
-            emergencyContactName, emergencyContactPhone
-        );
-        emergencyInfo.setResponsiveSteps(
-            new FormLayout.ResponsiveStep("0", 1),
-            new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2)
-        );
-        
+        emergencyInfo.add(emergencyContactName, emergencyContactPhone);
+        emergencyInfo.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep(RESPONSIVE_STEP_WIDTH, 2));
+
         // Setup combo boxes
         gender.setItems(Gender.values());
         gender.setItemLabelGenerator(Gender::name);
-        
+
         employeeRole.setItems(EmployeeRole.values());
         employeeRole.setItemLabelGenerator(EmployeeRole::getDisplayName);
-        
+
         // Configure number fields
         salary.setMin(0);
         salary.setValue(0.0);
         salary.setSuffixComponent(new TextField().getPrefixComponent());
-        
+
         // Configure text fields
         username.setPlaceholder("Ej: john.doe");
 
         phoneNumber.setPlaceholder("Ej: 809-555-5555");
 
         VerticalLayout content = new VerticalLayout();
-        content.add(
-            new H3("Información del Usuario"),
-            userInfo,
-            new H3("Dirección"),
-            addressInfo,
-            new H3("Información del Empleado"),
-            employeeInfo,
-            new H3("Contacto de Emergencia"),
-            emergencyInfo
-        );
-        
+        content.add(new H3("Información del Usuario"), userInfo, new H3("Dirección"), addressInfo,
+                new H3("Información del Empleado"), employeeInfo, new H3("Contacto de Emergencia"), emergencyInfo);
+
         content.addClassNames(LumoUtility.Padding.MEDIUM);
-        
+
         // Configure buttons
         saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        
+
         HorizontalLayout buttonLayout = new HorizontalLayout(cancelButton, saveButton);
         buttonLayout.addClassNames(LumoUtility.JustifyContent.END, LumoUtility.Gap.MEDIUM);
-        
+
         add(content, buttonLayout);
     }
 
@@ -171,35 +147,37 @@ public class EmployeeForm extends Dialog {
         username.setRequiredIndicatorVisible(true);
         username.setMinLength(MIN_USERNAME_LENGTH);
         username.setMaxLength(MAX_USERNAME_LENGTH);
-        username.setErrorMessage("El nombre de usario es requerido y debe tener entre " + MIN_USERNAME_LENGTH + " y " + MAX_USERNAME_LENGTH + " caracteres");
-        
+        username.setErrorMessage("El nombre de usario es requerido y debe tener entre " + MIN_USERNAME_LENGTH + " y "
+                + MAX_USERNAME_LENGTH + " caracteres");
+
         password.setRequired(true);
         password.setRequiredIndicatorVisible(true);
         password.setMinLength(MIN_PASSWORD_LENGTH);
-        password.setErrorMessage("La contraseña es requerida y debe tener al menos " + MIN_PASSWORD_LENGTH + " caracteres");
-        
+        password.setErrorMessage(
+                "La contraseña es requerida y debe tener al menos " + MIN_PASSWORD_LENGTH + " caracteres");
+
         firstName.setRequired(true);
         firstName.setRequiredIndicatorVisible(true);
         firstName.setErrorMessage("El nombre es requerido");
-        
+
         lastName.setRequired(true);
         lastName.setRequiredIndicatorVisible(true);
         lastName.setErrorMessage("El apellido es requerido");
-        
+
         email.setRequired(true);
         email.setRequiredIndicatorVisible(true);
         email.setPattern(EMAIL_PATTERN);
         email.setErrorMessage("El correo electrónico es requerido y debe ser válido");
-        
+
         phoneNumber.setRequired(true);
         phoneNumber.setRequiredIndicatorVisible(true);
         phoneNumber.setPattern(DOMINICAN_PHONE_PATTERN);
         phoneNumber.setErrorMessage("El teléfono debe ser en formato dominicano");
-        
+
         birthDate.setRequired(true);
         birthDate.setRequiredIndicatorVisible(true);
         birthDate.setErrorMessage("La fecha de nacimiento es requerida");
-        
+
         gender.setRequired(true);
         gender.setRequiredIndicatorVisible(true);
         gender.setErrorMessage("El género es requerido");
@@ -207,28 +185,28 @@ public class EmployeeForm extends Dialog {
         sector.setRequired(true);
         sector.setRequiredIndicatorVisible(true);
         sector.setErrorMessage("El sector es requerido");
-        
+
         streetAddress.setRequired(true);
         streetAddress.setRequiredIndicatorVisible(true);
         streetAddress.setErrorMessage("La dirección es requerida");
-        
+
         employeeRole.setRequired(true);
         employeeRole.setRequiredIndicatorVisible(true);
         employeeRole.setErrorMessage("El rol es requerido");
-        
+
         salary.setRequired(true);
         salary.setRequiredIndicatorVisible(true);
         salary.setErrorMessage("El salario es requerido");
-        
+
         hireDate.setRequired(true);
         hireDate.setRequiredIndicatorVisible(true);
         hireDate.setErrorMessage("La fecha de contratación es requerida");
-        
+
         workSchedule.setRequired(true);
         workSchedule.setRequiredIndicatorVisible(true);
         workSchedule.setErrorMessage("El horario laboral es requerido");
     }
-    
+
     private void setupEventListeners() {
         saveButton.addClickListener(this::save);
         cancelButton.addClickListener(e -> {
@@ -236,7 +214,7 @@ public class EmployeeForm extends Dialog {
             close();
         });
     }
-    
+
     private void save(ClickEvent<Button> event) {
         try {
             // Manual validation
@@ -244,169 +222,159 @@ public class EmployeeForm extends Dialog {
                 NotificationUtils.error("Por favor, complete todos los campos requeridos");
                 return;
             }
-            
-            EmployeeCreateRequestDto dto = EmployeeCreateRequestDto.builder()
-                .username(username.getValue())
-                .password(password.getValue())
-                .firstName(firstName.getValue())
-                .lastName(lastName.getValue())
-                .email(email.getValue())
-                .phoneNumber(phoneNumber.getValue())
-                .birthDate(birthDate.getValue())
-                .gender(gender.getValue())
-                .nationality(nationality.getValue())
-                .province(province.getValue())
-                .municipality(municipality.getValue())
-                .sector(sector.getValue())
-                .streetAddress(streetAddress.getValue())
-                .employeeRole(employeeRole.getValue())
-                .salary(salary.getValue())
-                .hireDate(hireDate.getValue())
-                .workSchedule(workSchedule.getValue())
-                .emergencyContactName(emergencyContactName.getValue())
-                .emergencyContactPhone(emergencyContactPhone.getValue())
-                .build();
-            
+
+            EmployeeCreateRequestDto dto = EmployeeCreateRequestDto.builder().username(username.getValue())
+                    .password(password.getValue()).firstName(firstName.getValue()).lastName(lastName.getValue())
+                    .email(email.getValue()).phoneNumber(phoneNumber.getValue()).birthDate(birthDate.getValue())
+                    .gender(gender.getValue()).nationality(nationality.getValue()).province(province.getValue())
+                    .municipality(municipality.getValue()).sector(sector.getValue())
+                    .streetAddress(streetAddress.getValue()).employeeRole(employeeRole.getValue())
+                    .salary(salary.getValue()).hireDate(hireDate.getValue()).workSchedule(workSchedule.getValue())
+                    .emergencyContactName(emergencyContactName.getValue())
+                    .emergencyContactPhone(emergencyContactPhone.getValue()).build();
+
             employeeService.save(dto);
             NotificationUtils.success("Empleado creado exitosamente");
-            
+
             // Fire the event with the created DTO
             fireEmployeeSavedEvent(dto);
-            
+
             if (onSaveCallback != null) {
                 onSaveCallback.run();
             }
-            
+
             close();
         } catch (Exception e) {
             log.error("Error creating employee", e);
             NotificationUtils.error("Error al crear empleado: " + e.getMessage());
         }
     }
-    
+
     /**
      * Validates all required form fields
+     * 
      * @return true if all required fields are valid, false otherwise
      */
     private boolean validateForm() {
         boolean isValid = true;
-        
+
         // Validate required fields
-        if (username.isEmpty() || username.getValue().length() < MIN_USERNAME_LENGTH || username.getValue().length() > MAX_USERNAME_LENGTH) {
+        if (username.isEmpty() || username.getValue().length() < MIN_USERNAME_LENGTH
+                || username.getValue().length() > MAX_USERNAME_LENGTH) {
             username.setInvalid(true);
             isValid = false;
         } else {
             username.setInvalid(false);
         }
-        
+
         if (password.isEmpty() || password.getValue().length() < MIN_PASSWORD_LENGTH) {
             password.setInvalid(true);
             isValid = false;
         } else {
             password.setInvalid(false);
         }
-        
+
         if (firstName.isEmpty()) {
             firstName.setInvalid(true);
             isValid = false;
         } else {
             firstName.setInvalid(false);
         }
-        
+
         if (lastName.isEmpty()) {
             lastName.setInvalid(true);
             isValid = false;
         } else {
             lastName.setInvalid(false);
         }
-        
+
         if (email.isEmpty() || !email.getValue().contains("@")) {
             email.setInvalid(true);
             isValid = false;
         } else {
             email.setInvalid(false);
         }
-        
+
         if (phoneNumber.isEmpty()) {
             phoneNumber.setInvalid(true);
             isValid = false;
         } else {
             phoneNumber.setInvalid(false);
         }
-        
+
         if (birthDate.isEmpty()) {
             birthDate.setInvalid(true);
             isValid = false;
         } else {
             birthDate.setInvalid(false);
         }
-        
+
         if (gender.isEmpty()) {
             gender.setInvalid(true);
             isValid = false;
         } else {
             gender.setInvalid(false);
         }
-        
+
         if (province.isEmpty()) {
             province.setInvalid(true);
             isValid = false;
         } else {
             province.setInvalid(false);
         }
-        
+
         if (municipality.isEmpty()) {
             municipality.setInvalid(true);
             isValid = false;
         } else {
             municipality.setInvalid(false);
         }
-        
+
         if (sector.isEmpty()) {
             sector.setInvalid(true);
             isValid = false;
         } else {
             sector.setInvalid(false);
         }
-        
+
         if (streetAddress.isEmpty()) {
             streetAddress.setInvalid(true);
             isValid = false;
         } else {
             streetAddress.setInvalid(false);
         }
-        
+
         if (employeeRole.isEmpty()) {
             employeeRole.setInvalid(true);
             isValid = false;
         } else {
             employeeRole.setInvalid(false);
         }
-        
+
         if (salary.isEmpty() || salary.getValue() < 0) {
             salary.setInvalid(true);
             isValid = false;
         } else {
             salary.setInvalid(false);
         }
-        
+
         if (hireDate.isEmpty()) {
             hireDate.setInvalid(true);
             isValid = false;
         } else {
             hireDate.setInvalid(false);
         }
-        
+
         if (workSchedule.isEmpty()) {
             workSchedule.setInvalid(true);
             isValid = false;
         } else {
             workSchedule.setInvalid(false);
         }
-        
+
         return isValid;
     }
-    
+
     public void openForNew() {
         // Clear all fields and reset form
         clearForm();
@@ -418,7 +386,7 @@ public class EmployeeForm extends Dialog {
         username.focus();
         open();
     }
-    
+
     /**
      * Clears all form fields
      */
@@ -443,7 +411,7 @@ public class EmployeeForm extends Dialog {
         emergencyContactName.clear();
         emergencyContactPhone.clear();
     }
-    
+
     /**
      * Enables all form fields for data entry
      */
@@ -469,7 +437,7 @@ public class EmployeeForm extends Dialog {
         emergencyContactPhone.setEnabled(true);
         saveButton.setEnabled(true);
     }
-    
+
     /**
      * Resets validation states for all fields
      */
@@ -491,31 +459,37 @@ public class EmployeeForm extends Dialog {
         hireDate.setInvalid(false);
         workSchedule.setInvalid(false);
     }
-    
+
     /**
      * Adds a listener that will be called when an employee is successfully saved.
-     * @param listener Consumer that receives the saved employee DTO
+     * 
+     * @param listener
+     *            Consumer that receives the saved employee DTO
      */
     public void addEmployeeSavedListener(Consumer<EmployeeCreateRequestDto> listener) {
         employeeSavedListeners.add(listener);
     }
-    
+
     /**
      * Adds a listener that will be called when the form is cancelled.
-     * @param listener Runnable to execute on cancel
+     * 
+     * @param listener
+     *            Runnable to execute on cancel
      */
     public void addEmployeeCancelledListener(Runnable listener) {
         employeeCancelledListeners.add(listener);
     }
-    
+
     /**
      * Notifies all saved listeners that an employee was successfully saved.
-     * @param dto The saved employee DTO
+     * 
+     * @param dto
+     *            The saved employee DTO
      */
     private void fireEmployeeSavedEvent(EmployeeCreateRequestDto dto) {
         employeeSavedListeners.forEach(listener -> listener.accept(dto));
     }
-    
+
     /**
      * Notifies all cancelled listeners that the form was cancelled.
      */
