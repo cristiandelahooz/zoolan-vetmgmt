ae5c3a9bff6b733a6e2b1fd742b21df1afab5428
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-07-30 03:03:22 -0400

    feat(warehouse): add WarehouseUpdateRequestDto for updating warehouse details

---
+++ src/main/java/com/wornux/dto/request/WarehouseUpdateRequestDto.java
@@ -0,0 +1,34 @@
+package com.wornux.dto.request;
+
+import com.wornux.data.entity.Product;
+import com.wornux.data.enums.WarehouseType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
+import static com.wornux.constants.ValidationConstants.MAX_WAREHOUSE_NAME_LENGTH;
+
+@Data
+@Builder
+public class WarehouseUpdateRequestDto {
+    @NotBlank(message = "El nombre del almacén es obligatorio")
+    @Size(min = 1, max = MAX_WAREHOUSE_NAME_LENGTH, message = "El nombre del almacén debe tener entre {min} y {max} caracteres")
+    private String name;
+
+    @NotNull(message = "El tipo de almacén es obligatorio")
+    private WarehouseType warehouseType;
+
+    @Nullable
+    private Boolean status;
+
+    @Nullable
+    private Boolean availableForSale;
+
+    @Nullable
+    private List<Product> products;
+}
