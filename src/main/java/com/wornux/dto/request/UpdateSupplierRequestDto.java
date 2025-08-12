82b9cee91090844db012eefe15a67a8f47f359da
carolinabencosme <cbencome@gmail.com>
2025-08-12 19:45:55 -0400

    fix(UpdateSupplierRequestDto): update RNC validation message to include 11 digit format

--- src/main/java/com/wornux/dto/request/UpdateSupplierRequestDto.java
+++ src/main/java/com/wornux/dto/request/UpdateSupplierRequestDto.java
@@ -1,39 +1,37 @@
 package com.wornux.dto.request;
 
-import static com.wornux.constants.ValidationConstants.DOMINICAN_PHONE_PATTERN;
-import static com.wornux.constants.ValidationConstants.RNC_PATTERN;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+import static com.wornux.constants.ValidationConstants.DOMINICAN_PHONE_PATTERN;
+import static com.wornux.constants.ValidationConstants.RNC_PATTERN;
+
 @Data
 public class UpdateSupplierRequestDto {
 
-  private Long id;
+    private Long id;
 
-  @Pattern(regexp = RNC_PATTERN, message = "El RNC debe contener exactamente 9 dígitos")
-  private String rnc;
+    @Pattern(regexp = RNC_PATTERN, message = "El RNC debe contener exactamente 9 dígitos o  11 dígitos")
+    private String rnc;
 
-  private String companyName;
+    private String companyName;
 
-  private String contactPerson;
+    private String contactPerson;
 
-  @Pattern(
-      regexp = DOMINICAN_PHONE_PATTERN,
-      message = "Proporcione un número de teléfono válido (809, 849 o 829 seguido de 7 dígitos)")
-  private String contactPhone;
+    @Pattern(regexp = DOMINICAN_PHONE_PATTERN, message = "Proporcione un número de teléfono válido (809, 849 o 829 seguido de 7 dígitos)")
+    private String contactPhone;
 
-  @Email(message = "Por favor, proporcione una dirección de correo electrónico válida")
-  private String contactEmail;
+    @Email(message = "Por favor, proporcione una dirección de correo electrónico válida")
+    private String contactEmail;
 
-  private String province;
+    private String province;
 
-  private String municipality;
+    private String municipality;
 
-  private String sector;
+    private String sector;
 
-  private String streetAddress;
+    private String streetAddress;
 
-  private Boolean active;
+    private Boolean active;
 }
