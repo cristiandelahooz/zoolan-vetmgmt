1a083cc9c56394af8d4afd4de4a0ebd21cd3a2b0
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-08-13 14:18:29 -0400

    refactor(warehouse): add cascade and orphanRemoval to products relationship for better data integrity

--- src/main/java/com/wornux/data/entity/Warehouse.java
+++ src/main/java/com/wornux/data/entity/Warehouse.java
@@ -1,14 +1,16 @@
 package com.wornux.data.entity;
 
-import static com.wornux.constants.ValidationConstants.MAX_WAREHOUSE_NAME_LENGTH;
 
 import com.wornux.data.enums.WarehouseType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
-import java.util.List;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.envers.Audited;
+
+import java.util.List;
+
+import static com.wornux.constants.ValidationConstants.MAX_WAREHOUSE_NAME_LENGTH;
 
 @Entity
 @Table(name = "warehouses")
@@ -20,28 +22,20 @@ import org.hibernate.envers.Audited;
 @Slf4j
 @Audited(withModifiedFlag = true)
 public class Warehouse {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-
-  @Column(name = "warehouse_name", nullable = false, length = MAX_WAREHOUSE_NAME_LENGTH)
-  @Size(
-      min = 1,
-      max = MAX_WAREHOUSE_NAME_LENGTH,
-      message = "El nombre del almacén debe tener entre {min} y {max} caracteres")
-  private String name;
-
-  @Column(name = "warehouse_type", nullable = false)
-  @Enumerated(EnumType.STRING)
-  private WarehouseType warehouseType;
-
-  @Column(name = "status", nullable = false)
-  @Builder.Default
-  private boolean status = true;
-
-  @Column(name = "is_available_for_sale")
-  private boolean availableForSale;
-
-  @OneToMany(mappedBy = "warehouse")
-  private List<Product> products;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "warehouse_name", nullable = false, length = MAX_WAREHOUSE_NAME_LENGTH)
+    @Size(min = 1, max = MAX_WAREHOUSE_NAME_LENGTH, message = "El nombre del almacén debe tener entre {min} y {max} caracteres")
+    private String name;
+    @Column(name = "warehouse_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private WarehouseType warehouseType;
+    @Column(name = "status", nullable = false)
+    @Builder.Default
+    private boolean status = true;
+    @Column(name = "is_available_for_sale")
+    private boolean availableForSale;
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
 }
