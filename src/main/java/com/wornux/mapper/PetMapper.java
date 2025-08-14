554c8aefa740a7246dcfd3343953b0af44434427
carolinabencosme <cbencome@gmail.com>
2025-08-14 14:31:08 -0400

    fix(PetMapper): ignore owners mapping in updatePetFromDTO method

--- src/main/java/com/wornux/mapper/PetMapper.java
+++ src/main/java/com/wornux/mapper/PetMapper.java
@@ -1,32 +1,28 @@
 package com.wornux.mapper;
 
+import com.wornux.data.entity.Client;
+
 import com.wornux.data.entity.Pet;
 import com.wornux.dto.request.PetCreateRequestDto;
 import com.wornux.dto.request.PetUpdateRequestDto;
-import com.wornux.mapper.helper.PetMapperHelper;
 import org.mapstruct.*;
+import com.wornux.mapper.helper.PetMapperHelper;
 
-@Mapper(
-    componentModel = "spring",
-    unmappedTargetPolicy = ReportingPolicy.IGNORE,
-    uses = PetMapperHelper.class)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = PetMapperHelper.class)
 public interface PetMapper {
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "owners", source = "dto.ownerId", qualifiedByName = "mapOwnerAsList")
-  @Mapping(target = "birthDate", source = "dto.birthDate")
-  @Mapping(target = "gender", source = "dto.gender")
-  @Mapping(target = "color", source = "dto.color")
-  @Mapping(target = "size", source = "dto.size")
-  @Mapping(target = "furType", source = "dto.furType")
-  Pet toEntity(PetCreateRequestDto dto);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "owners", source = "dto.ownerId", qualifiedByName = "mapOwnerAsList")
+    @Mapping(target = "birthDate", source = "dto.birthDate")
+    @Mapping(target = "gender", source = "dto.gender")
+    @Mapping(target = "color", source = "dto.color")
+    @Mapping(target = "size", source = "dto.size")
+    @Mapping(target = "furType", source = "dto.furType")
+    Pet toEntity(PetCreateRequestDto dto);
 
-  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-  @Mapping(target = "owners", source = "dto.ownerId", qualifiedByName = "mapOwnerAsList")
-  @Mapping(target = "color", source = "dto.color")
-  @Mapping(target = "size", source = "dto.size")
-  @Mapping(target = "furType", source = "dto.furType")
-  void updatePetFromDTO(PetUpdateRequestDto dto, @MappingTarget Pet pet);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "owners", ignore = true)
+    void updatePetFromDTO(PetUpdateRequestDto dto, @MappingTarget Pet pet);
 
-  PetCreateRequestDto toCreateDTO(Pet savedPet);
+    PetCreateRequestDto toCreateDTO(Pet savedPet);
 }
