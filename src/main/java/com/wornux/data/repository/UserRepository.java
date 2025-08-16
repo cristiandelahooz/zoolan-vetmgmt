eecec7de97c4f065bb8ec8185ba8b76cf48d8fff
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-08-16 00:28:31 -0400

    refactor(user): add existsByEmail method to check email existence

--- src/main/java/com/wornux/data/repository/UserRepository.java
+++ src/main/java/com/wornux/data/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends AbstractRepository<User, Long> {
   Optional<User> findByUsername(String username);
 
   Optional<User> findByEmail(String email);
+  boolean existsByEmail(String email);
 }
