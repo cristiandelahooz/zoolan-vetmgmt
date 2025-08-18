26cb0a1bc12fda026cd1057dfd40c92f00891fe4
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-08-18 19:46:28 -0400

    refactor(InvoiceServiceRepository): remove unused methods and imports for cleaner code

--- src/main/java/com/wornux/data/repository/InvoiceServiceRepository.java
+++ src/main/java/com/wornux/data/repository/InvoiceServiceRepository.java
@@ -1,15 +1,9 @@
 package com.wornux.data.repository;
 
-import com.wornux.data.entity.Invoice;
 import com.wornux.data.entity.ServiceInvoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Repository for InvoiceService entity operations
@@ -17,10 +11,4 @@ import java.util.Optional;
 @Repository
 public interface InvoiceServiceRepository extends JpaRepository<ServiceInvoice, Long>, JpaSpecificationExecutor<ServiceInvoice> {
 
-  List<ServiceInvoice> findByInvoiceCode(Long invoiceCode);
-
-  void deleteByInvoiceCode(Long invoiceCode);
-
-  @Query("SELECT i FROM Invoice i LEFT JOIN FETCH i.services WHERE i.code = :id")
-  Optional<Invoice> findByIdWithServices(@Param("id") Long id);
 }
