45688be5dd5a3bbad1ee3e7c59cf96bb9754df56
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-08-18 19:46:15 -0400

    feat(InvoiceRepository): add method to find invoice by code with services and products

--- src/main/java/com/wornux/data/repository/InvoiceRepository.java
+++ src/main/java/com/wornux/data/repository/InvoiceRepository.java
@@ -1,8 +1,17 @@
 package com.wornux.data.repository;
 
 import com.wornux.data.entity.Invoice;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InvoiceRepository
-    extends JpaRepository<Invoice, Long>, JpaSpecificationExecutor<Invoice> {}
+    extends JpaRepository<Invoice, Long>, JpaSpecificationExecutor<Invoice> {
+
+  @EntityGraph(attributePaths = {"client", "products.product", "services.service"})
+  @Query("SELECT i FROM Invoice i WHERE i.code = :code")
+  Optional<Invoice> findByCodeWithServicesAndProducts(@Param("code") Long code);
+}
