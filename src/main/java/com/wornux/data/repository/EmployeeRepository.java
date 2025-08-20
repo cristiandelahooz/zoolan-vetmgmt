32a29e34775b869477bfa71a5a117ba0a654a157
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-08-20 14:44:05 -0400

    refactor(EmployeeRepository): update method to find all available employees with pagination support

--- src/main/java/com/wornux/data/repository/EmployeeRepository.java
+++ src/main/java/com/wornux/data/repository/EmployeeRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -62,8 +63,9 @@ public interface EmployeeRepository
   /**
    * Finds all employees with availability.
    */
+
   @Query("SELECT e FROM Employee e WHERE e.available = true")
-  List<Employee> findByAvailableTrue();
+  Page<Employee> findAllAvailable(Specification<Employee> spec, Pageable pageable);
 
   /**
    * Finds all employees by salary range.
