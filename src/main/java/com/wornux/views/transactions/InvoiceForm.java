b7e894e53e657450a70177e6803a936416e0c300
Manuel Jose Rodriguez Cruz <138523061+manuujrodcruz@users.noreply.github.com>
2025-08-25 17:35:37 +0000

    feat(invoice): enhance customer selection with detailed display and active clients filter

--- src/main/java/com/wornux/views/transactions/InvoiceForm.java
+++ src/main/java/com/wornux/views/transactions/InvoiceForm.java
@@ -746,7 +746,24 @@ public class InvoiceForm extends Div {
               c.addClassNames(LumoUtility.Margin.Horizontal.XSMALL);
             });
 
+    List<Client> allCustomerByDisabledIsFalse = customerService.getAllActiveClients();
     customer.setClearButtonVisible(true);
+    customer.setItemLabelGenerator(Client::getFirstName);
+    customer.setRenderer(
+        new ComponentRenderer<>(
+            item -> {
+              Div container = new Div();
+              container.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN);
+
+              Span title = new Span(item.getFirstName());
+              title.addClassNames(LumoUtility.FontWeight.BOLD);
+
+              Span subtitle = new Span(item.getFullName());
+              subtitle.addClassNames(LumoUtility.TextColor.SECONDARY, LumoUtility.FontSize.SMALL);
+
+              container.add(title, subtitle);
+              return container;
+            }));
     customer.addValueChangeListener(
         event -> {
           bill.setVisible(event.getValue() != null);
